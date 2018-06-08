6df76c451ec680dff66b56fd8778214699dc47e7
Zachary Schwartz <zls@Zacharys-MacBook.local>
2018-06-07 22:43:13 -0700

    adds items

--- router.go
+++ router.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/NYTimes/gziphandler"
 )
 
 func getRouter() *http.ServeMux {
 	mux := http.NewServeMux()
+	// gzip assets:
+	assetHandler := gziphandler.GzipHandler(http.StripPrefix("/static/", http.FileServer(http.Dir("./web/owlio-spa/build/static"))))
+
 	// Serve spa application:
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/owlio-spa/build/static"))))
+	mux.Handle("/static/", assetHandler)
 
 	mux.HandleFunc("/service-worker.js", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/owlio-spa/build/service-worker.js")
