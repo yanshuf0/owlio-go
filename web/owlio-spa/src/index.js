13864847f3087d6c32b6db62e74c464e5ee5e9d4
Zachary Schwartz <zls@Zacharys-MacBook.local>
2018-06-08 08:28:57 -0700

    removes unused function

--- web/owlio-spa/src/index.js
+++ web/owlio-spa/src/index.js
@@ -4,24 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createMuiTheme } from '@material-ui/core/styles';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
-    },
-  },
-});
