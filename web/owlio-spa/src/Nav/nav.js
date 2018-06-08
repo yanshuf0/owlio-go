a0d625387b9683845f57564a716a1207cc1d741d
Zachary Schwartz <zls@Zacharys-MacBook.local>
2018-06-08 07:55:50 -0700

    menu search

--- web/owlio-spa/src/Nav/nav.js
+++ web/owlio-spa/src/Nav/nav.js
@@ -1,36 +1,43 @@
 import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-import './nav.css'
+import "./nav.css";
+import Input from "@material-ui/core/Input";
 
 export default class Nav extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            position: '0em'
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      position: "0em"
+    };
+  }
 
-    slide = () => {
-        this.setState({
-            position: this.state.position === '0em' ? '-12em' : '0em'
-        });
-    }
+  slide = () => {
+    this.setState({
+      position: this.state.position === "0em" ? "-10.2em" : "0em"
+    });
+  };
+
   render() {
     return (
       <Navbar color="dark" dark expand="md">
         <NavbarBrand href="/">owlio.io</NavbarBrand>
-        <span className="filler"></span>
+        <span className="filler" />
         <span onClick={this.slide}>
           <svg
+            style={{ transform: `translate(${this.state.position})` }}
             id="circle"
             viewBox="0 0 24 24"
-            style={{transform: `translate(${this.state.position})`}}
           >
             <path
               fill="white"
               d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
             />
           </svg>
+        </span>
+        <span id="searchSpan">
+          {this.state.position !== "0em" && (
+            <Input id="searchInput" type="text" color="secondary"></Input>
+          )}
         </span>
       </Navbar>
     );
