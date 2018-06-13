beafba1eb5839b02a2639f09735f3d6caa0ce67c
Zachary Schwartz <zachscs@outlook.com>
2018-06-13 16:02:30 -0700

    setup api routing

--- router.go
+++ router.go
@@ -12,13 +12,11 @@ func getRouter() *mux.Router {
 	mux := mux.NewRouter()
 	// Create G-Zip asset handler:
 	assetHandler := gziphandler.GzipHandler(http.StripPrefix("/static/", http.FileServer(http.Dir("./web/owlio-spa/build/static"))))
-
+	// Interceipt service-worker request:
+	serviceWorkerHandler := http.StripPrefix("/service-worker.js", http.FileServer(http.Dir("./web/owlio-spa/build")))
 	// Start spa:
 	mux.PathPrefix("/static/").Handler(assetHandler)
-
-	mux.HandleFunc("/service-worker.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./web/owlio-spa/build/service-worker.js")
-	})
+	mux.PathPrefix("/service-worker.js").Handler(serviceWorkerHandler)
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/owlio-spa/build/index.html")
