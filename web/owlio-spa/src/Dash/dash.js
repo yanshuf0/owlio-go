cc583a6172fac13f6afca812e4fdbc96a80df87a
Zachary Schwartz <zls@Zacharys-MacBook.local>
2018-06-13 13:38:36 -0700

    removing bad imports

--- web/owlio-spa/src/Dash/dash.js
+++ web/owlio-spa/src/Dash/dash.js
@@ -1,6 +1,6 @@
 import React from "react";
 import InfiniteScroll from "react-infinite-scroller";
-import { Card, CardTitle, CardText, Button } from "reactstrap";
+import { Card, CardTitle, CardText, Button, Row, Col } from "reactstrap";
 
 export default class DashComponent extends React.Component {
   constructor() {
@@ -11,37 +11,45 @@ export default class DashComponent extends React.Component {
   }
 
   loadHoots = () => {
-      for (let i = 0; i < 8; i++) {
-        this.setState({
-          hoots: this.state.hoots.concat('lorem ipsum')
-        })
-      }
+    for (let i = 0; i < 8; i++) {
+      this.setState({
+        hoots: this.state.hoots.concat("lorem ipsum")
+      });
+    }
   };
 
   render() {
     const items = [];
-    this.state.hoots.map((hoot, i) => 
+    this.state.hoots.map((hoot, i) =>
       items.push(
-        <Card
-          body
-          inverse
-          style={{ backgroundColor: "#333", borderColor: "#333" }}
-          key={i}
-        >
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>
-            {hoot}
-          </CardText>
-          <Button>Button</Button>
-        </Card>
+        <Row key={i}>
+          <Col xs="6">
+            <Card
+              body
+              inverse
+              style={{ backgroundColor: "#333", borderColor: "#333" }}
+            >
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>{hoot}</CardText>
+              <Button>Button</Button>
+            </Card>
+          </Col>
+          <Col xs="6">
+            <Card
+              body
+              inverse
+              style={{ backgroundColor: "#333", borderColor: "#333" }}
+            >
+              <CardTitle>Special Title Treatment</CardTitle>
+              <CardText>{hoot}</CardText>
+              <Button>Button</Button>
+            </Card>
+          </Col>
+        </Row>
       )
     );
     return (
-      <InfiniteScroll
-        pageStart={0}
-        loadMore={this.loadHoots}
-        hasMore={true}
-      >
+      <InfiniteScroll pageStart={0} loadMore={this.loadHoots} hasMore={true}>
         {items}
       </InfiniteScroll>
     );
