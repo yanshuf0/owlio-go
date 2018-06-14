baa0984878a77fdc56c658db9021d729e82939af
Zachary Schwartz <zachscs@outlook.com>
2018-06-14 10:10:11 -0700

    adds email to user

--- models/user/user_model.go
+++ models/user/user_model.go
@@ -18,6 +18,7 @@ type ContextKey string
 type User struct {
 	ID       bson.ObjectId `json:"id" bson:"_id,omitempty"`
 	Username string        `json:"username" bson:"username"`
+	Email    string        `json:"email" bson:"email"`
 	Password string        `json:"password" bson:"password"`
 }
 
